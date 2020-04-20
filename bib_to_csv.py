d5a6f8ad739e464a765f7227476e76a08c576ca0
Jason McEwen <jason.mcewen@gmail.com>
2020-04-20 09:29:13 +0100

    Fix bug with file close and revise formatting

--- bib_to_csv.py
+++ bib_to_csv.py
@@ -3,38 +3,47 @@ import string
 
 #open a bibtex file
 parser = bibtex.Parser()
-bibdata = parser.parse_file("bibliography.bib")
+bibdata = parser.parse_file("/Users/mcewen/AP/Docs/Bibtex/mybibs_new.bib")
 
 # tralsator for removing punctuation
 translator = str.maketrans('', '', string.punctuation)
 
 # open our output file
-f = open('my_bib.csv', 'w') 
+f = open('mybibs_new.csv', 'w') 
 
 # header row
-f.write("title\t year\t author\t journal\n")
+f.write("Title, Year, Authors, Journal, Arxiv\n")
 
 #loop through the individual references
 for bib_id in bibdata.entries:
     b = bibdata.entries[bib_id].fields
     try:
         # change these lines to create a SQL insert
-        f.write(b["title"])
-        f.write(" \t")
+        title = b["title"]
+        title = title.replace('{', '')
+        title = title.replace('}', '')
+        f.write('"' + title + '"')
+        f.write(" ,")
         f.write(b["year"])
-        f.write(" \t")
+        f.write(" ,")
         #deal with multiple authors
         authors = ""
         for author in bibdata.entries[bib_id].persons["author"]:
             new_author = str(author.first()) + " " + str(author.last()) 
             new_author = new_author.translate(translator)
+            new_author = new_author.replace('{', '')
+            new_author = new_author.replace('}', '')
             if len(authors) == 0:
                 authors = '"' + new_author
             else:
                 authors = authors + ", " + new_author
         f.write(authors + '"')
-        f.write(" \t")
-        f.write(b["journal"])
+        f.write(" ,")
+        if 'eprint' in b.keys():
+            f.write(b["eprint"])
+        f.write(" ,")
+        f.write('"' + b["journal"] + '"')
+
     # field may not exist for a reference
     except(KeyError):
         f.write(b["booktitle"])
@@ -42,5 +51,5 @@ for bib_id in bibdata.entries:
         continue
     f.write("\n")
     
-    # close output file
-    f.close()
+# close output file
+f.close()
